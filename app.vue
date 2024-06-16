5fefd40e7160cc756891a653b70a9e72bb15e046
Andrew Alba <andrew.alba@gmail.com>
2024-06-15 22:58:44 -0500

    🎨 style(app.vue): remove commented out styles and scripts for cleaner code ✨ feat(app.vue): add various sizes of apple-touch-icon and android-icon for better device compatibility 📦 feat(assets): add new icon files for apple and android devices 🔧 fix(browserconfig.xml): add new browserconfig.xml for better browser compatibility 🔄 fix(favicon.ico): update favicon.ico for updated site branding
    ✨ feat(manifest.json): add manifest file with app icons for different densities to improve app's PWA capabilities

--- app.vue
+++ app.vue
@@ -6,39 +6,92 @@ useHead({
   link: [
     {
       rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css?family=Luckiest+Guy|Open+Sans',
-    },
-    /*{
-      rel: 'stylesheet',
-      href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css',
+      href: 'https://fonts.googleapis.com/css?family=Luckiest+Guy|Open+Sans'
     },
     {
-      rel: 'stylesheet',
-      href: 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css',
-    },*/
+      rel: 'apple-touch-icon',
+      sizes: '57x57',
+      href: '/assets/icons/apple-icon-57x57.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '60x60',
+      href: '/assets/icons/apple-icon-60x60.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '72x72',
+      href: '/assets/icons/apple-icon-72x72.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '76x76',
+      href: '/assets/icons/apple-icon-76x76.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '114x114',
+      href: '/assets/icons/apple-icon-114x114.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '120x120',
+      href: '/assets/icons/apple-icon-120x120.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '144x144',
+      href: '/assets/icons/apple-icon-144x144.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '152x152',
+      href: '/assets/icons/apple-icon-152x152.png'
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/assets/icons/apple-icon-180x180.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '36x36',
+      href: '/assets/icons/android-icon-36x36.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '48x48',
+      href: '/assets/icons/android-icon-48x48.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '72x72',
+      href: '/assets/icons/android-icon-72x72.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '96x96',
+      href: '/assets/icons/android-icon-96x96.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '144x144',
+      href: '/assets/icons/android-icon-144x144.png'
+    },
+    ,
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      href: '/assets/icons/android-icon-192x192.png'
+    }
   ],
   script: [
-    {
-      id: 'jquery',
-      src: 'https://code.jquery.com/jquery-1.12.4.min.js',
-      integrity: 'sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=',
-      crossorigin: 'anonymous',
-      /*src: '/assets/js/jquery.min.js',*/
-      type: 'text/javascript',
-      async: true,
-    },
-    /*{
-      src: '/assets/js/jquery.dropotron.min.js',
-      type: 'text/javascript',
-      defer: true,
-      body: true
-    },
-    {
-      src: '/assets/js/jquery.scrollex.min.js',
-      type: 'text/javascript',
-      defer: true,
-      body: true
-    },*/
     {
       src: '/assets/js/browser.min.js',
       type: 'text/javascript',
