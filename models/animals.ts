d62da8774fec17ba4c853f7c73352f22384fb8e4
Andrew Alba <andrew.alba@gmail.com>
2025-12-06 17:41:16 -0600

    ♻️ (cleanup): remove unused components, models, and API logic to streamline the codebase and improve maintainability 📝 (env.example): update .env.example by removing Petfinder related variables as they are no longer needed ✨ (adopt/index.vue): integrate Petfinder's pet list widget for improved pet display functionality 🔧 (nuxt.config.ts): simplify runtime configuration by removing unnecessary Petfinder API settings ✅ (package.json): remove unused dependencies related to Petfinder to reduce bundle size and improve performance

--- models/animals.ts
+++
@@ -1,7 +0,0 @@
-import type { Animal } from "~/models/animal";
-import type { Pagination } from "~/models/pagination";
-
-export interface Animals {
-    animals:    Animal[];
-    pagination: Pagination;
-}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  runtimeConfig: {
-    petfinderApiSecret: process.env.PETFINDER_API_SECRET,
-    public: {
-      apiUrl: process.env.API_URL,
-      petfinderApiKey: process.env.PETFINDER_API_KEY,
-      petfinderBaseUrl: process.env.PETFINDER_BASE_URL,
-      petfinderOrgId: process.env.PETFINDER_ORGANIZATION_ID,
-      gtagId: process.env.GAG_ID,
-    }
-  },
-  ssr: false,
-  modules: [
-      '@pinia/nuxt',
-    'vue3-carousel-nuxt'
-  ],
-  plugins: [
-    '~/plugins/petfinder.ts'
-  ],
-  css: [
-    'assets/sass/main.scss'
-  ],
-  vite: {
-    build: {
-      minify: 'esbuild',
-      chunkSizeWarningLimit: 1000,
-      cssCodeSplit: true,
+    devtools: {enabled: true},
+    runtimeConfig: {
+        public: {
+            apiUrl: process.env.API_URL,
+            gtagId: process.env.GAG_ID,
+        }
     },
-    server: {
-      watch: {
-        usePolling: true,
-      },
+    ssr: false,
+    modules: [
+        '@pinia/nuxt',
+    ],
+    plugins: [],
+    css: [
+        'assets/sass/main.scss'
+    ],
+    vite: {
+        build: {
+            minify: 'esbuild',
+            chunkSizeWarningLimit: 1000,
+            cssCodeSplit: true,
+        },
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        }
     }
-  }
 })
