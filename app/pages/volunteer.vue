42d6e5401f05d98fa7ba74062443ac3bea403a42
Andrew Alba <andrew.alba@gmail.com>
2026-05-17 19:02:56 -0500

    📝 (cancel.vue, volunteer.vue): remove images from donate and volunteer pages to streamline content and focus on messaging

--- app/pages/volunteer.vue
+++ app/pages/volunteer.vue
@@ -21,7 +21,6 @@ const email = runtimeConfig.public.email
 
         <!-- Content -->
         <section id="content" class="box">
-          <span class="image fit"><img src="/images/volunteer.jpg" alt="volunteer" /></span>
           <p>Some of the things we can always use help with are: </p>
           <ul>
             <li>assisting with adoption days (must be at least 18 years old)</li>
